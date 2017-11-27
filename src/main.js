83febabd5d1e600341e33d2d9bca0f48108cea03
falcaoaugustos <falcaoaugustos@gmail.com>
2017-11-27 01:09:48 -0300

    feat (script): create memory manager algorithms placeholder

---
+++ src/main.js
@@ -0,0 +1,15 @@
+function fifo() {
+  alert("algoritmo FIFO");
+}
+
+function ssf() {
+  alert("algoritmo SSF");
+}
+
+function scan() {
+  alert("algoritmo SCAN");
+}
+
+function cscan() {
+  alert("algoritmo C-SCAN");
+}
